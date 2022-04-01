0b11a9af09fc34d084445f4d3b4b2c4f23401f93
enzocsantos18 <enzocsantos18@gmail.com>
2022-04-01 20:17:09 -0300

    2 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,40 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ClassesService } from './classes.service';
+import { TimerService } from './timer.service';
+import { ClassesComponent } from './classes/classes.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'timer',
+        component: TimerComponent,
+      },
+      {
+        path: 'classes',
+        component: ClassesComponent,
+      },
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    TimerComponent,
+    ClassesComponent,
+    HomeComponent,
+  ],
+  bootstrap: [AppComponent],
+  providers: [ClassesService, TimerService],
 })
-export class AppModule { }
+export class AppModule {}
